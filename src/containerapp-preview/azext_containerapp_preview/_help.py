1e1b2a7ec35b8a717f150b42c86372a853c30ee6
Steven Murawski <stmuraws@microsoft.com>
2022-03-25 14:24:57 -0500

    containerapp-preview initial commit

---
+++ src/containerapp-preview/azext_containerapp_preview/_help.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps  # pylint: disable=unused-import
+
+
+helps['containerapps compose'] = """
+    type: group
+    short-summary: Commands to manage Containerappss.
+"""
+
+helps['containerapps compose create'] = """
+    type: command
+    short-summary: Create a Containerapps.
+"""
