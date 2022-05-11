ad96e77ce72db1f6d66c57d0ca6c1ba6523d358c
Steven Murawski <stmuraws@microsoft.com>
2022-05-11 16:57:18 +0000

    make linter pass

--- src/containerapp-preview/azext_containerapp_preview/__init__.py
+++ src/containerapp-preview/azext_containerapp_preview/__init__.py
@@ -4,6 +4,7 @@
 # --------------------------------------------------------------------------------------------
 
 from azure.cli.core import AzCommandsLoader
+from ._help import helps  # pylint: disable=unused-import
 
 
 class ContainerappPreviewCommandsLoader(AzCommandsLoader):
